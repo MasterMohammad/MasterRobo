8401439f3b4ddae4cbf2b3bc60985a235ff46d35
MasterMohammad <mohammad_hmadani@yahoo.com>
2016-03-19 15:02:52 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {@MasterMohammad},--Sudo users
+    sudo_users = {74732940},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
